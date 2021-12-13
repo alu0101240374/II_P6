a27513cb39afd2170b8224a361824e844aa47ae5
alu0101240374 <alu0101240374@ull.edu.es>
2021-12-13 21:02:59 +0000

    entrega

--- Scripts/Multimodal.cs
+++ Scripts/Multimodal.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Compass : MonoBehaviour
+public class Multimodal : MonoBehaviour
 {
     public float speed = 10f;
     private bool localitationActivated = false;
